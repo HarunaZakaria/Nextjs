5225035015400ff7fe3350ae0cd6c1708aac1877
Harunzy <Harunzy55@gmail.com>
2026-06-27 17:46:15 -0700

    add a clear btn

--- qreate/app/components/QRInput.tsx
+++ qreate/app/components/QRInput.tsx
@@ -2,9 +2,10 @@ import React from "react";
 type QRInputProps = {
   text: string;
   setText: (text: string) => void;
+  onEnter?: () => void;
 };
 
-function QRInput({ text, setText }: QRInputProps) {
+function QRInput({ text, setText, onEnter }: QRInputProps) {
   return (
     <div>
       <input
@@ -12,6 +13,11 @@ function QRInput({ text, setText }: QRInputProps) {
         placeholder="Type something...."
         value={text}
         onChange={(e) => setText(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" && onEnter) {
+            onEnter();
+          }
+        }}    
         className="mt-6 w-full border rounder-lg p-3"
       />
     </div>
