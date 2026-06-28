0921a098b4538a60af4bff6a941637a5a1bba28e
Harunzy <Harunzy55@gmail.com>
2026-06-27 19:33:21 -0700

    add

--- qreate/app/page.tsx
+++ qreate/app/page.tsx
@@ -28,26 +28,33 @@ export default function Home() {
     setQrValue("");
     setError("");
   };
+
   //handle download QR code
   const downloadQRCode = () => {
     const svg = qrRef.current?.querySelector("svg");
+
     if (!svg) return;
+
     const svgData = new XMLSerializer().serializeToString(svg);
     const svgBlob = new Blob([svgData], {
       type: "image/svg+xml;charset=utf-8",
     });
+
     const url = URL.createObjectURL(svgBlob);
+
     const link = document.createElement("a");
     link.href = url;
     link.download = "qrcode.svg";
     link.click();
+
     URL.revokeObjectURL(url);
   };
+
   return (
     <main className="flex min-h-screen items-center justify-center bg-gray-100">
       <div className="bg-white  p-8 rounded-xl shadow-lg w-[450px]">
-        <h1 className="text-3xl font-bold"> QR Code Generator</h1>
-        <div className="mt-2 text-gray-500">Generate a QR code instantly</div>
+        <h1 className="text-3xl font-bold text-center text-gray-600"> QR Code Generator</h1>
+        <div className="mt-2 text-gray-500 text-center">Generate a QR code instantly</div>
         <QRInput text={text} setText={setText} onEnter={generateQRCode} />
         {error && <p className="mt-2 text-red-600">{error}</p>}
         <div className="mt-4 flex gap-4">
@@ -64,14 +71,16 @@ export default function Home() {
             Clear
           </button>
         </div>
-        <QRDisplay value={qrValue} ref={qrRef} />
-        <button
-          onClick={downloadQRCode}
-          disabled={!qrValue}
-          className="mt-4 w-full bg-green-500 text-white p-3  rounded-lg hover:bg-green-700"
-        >
-          {!qrValue ? "No QR code yet" : "Download QR Code"}
-        </button>
+        {qrValue && <QRDisplay value={qrValue} ref={qrRef} />}
+        {qrValue && (
+          <button
+            onClick={downloadQRCode}
+            disabled={!qrValue}
+            className="mt-4 w-full bg-green-500 text-white p-3  rounded-lg hover:bg-green-700"
+          >
+            {!qrValue ? "No QR code yet" : "Download QR Code"}
+          </button>
+        )}
       </div>
     </main>
   );
