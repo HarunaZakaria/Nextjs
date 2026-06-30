1326abb54043ee0d80d99f30454467cdb2002a26
Harunzy <Harunzy55@gmail.com>
2026-06-30 14:28:42 -0700

    add

--- qreate/app/components/QRInput.tsx
+++ qreate/app/components/QRInput.tsx
@@ -17,8 +17,8 @@ function QRInput({ text, setText, onEnter }: QRInputProps) {
           if (e.key === "Enter" && onEnter) {
             onEnter();
           }
-        }}    
-        className="mt-6 w-full border rounder-lg p-3"
+        }}
+        className="mt-6 w-full border rounder-lg p-3  focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
       />
     </div>
   );
