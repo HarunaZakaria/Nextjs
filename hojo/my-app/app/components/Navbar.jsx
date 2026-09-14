380f7127a8a5c70ac847f409dabe56833f315717
Harunzy <Harunzy55@gmail.com>
2026-09-13 23:12:10 -0700

    add

--- hojo/my-app/app/components/Navbar.jsx
+++ hojo/my-app/app/components/Navbar.jsx
@@ -1,13 +1,21 @@
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "./dojo-logo.png";
 
 export default function Navbar() {
   return (
-    <main>
-      <nav>
-        <h1>Next-app</h1>
-        <Link href="/">Dashboard</Link>
-        <Link href="/tickets">Tickets</Link>
-      </nav>
-    </main>
+    <nav>
+      <Image
+        src={Logo}
+        width={70}
+        height={100}
+        alt="Dojo logo"
+        quality={100}
+        placeholder="blur"
+      />
+      <h1>Next-app</h1>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets">Tickets</Link>
+    </nav>
   );
 }
